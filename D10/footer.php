7ec95e43e8063c43744594219973ba117b64acc6
iwanttika16 <97109870+iwanttika16@users.noreply.github.com>
2025-06-27 23:20:28 +0530

    Add files via upload

---
+++ D10/footer.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>footer</title>
+  <link rel="stylesheet" href="footer.css">
+</head>
+<body>
+  <footer>
+  <div class="footer-container">
+      <span class="footer-left">
+        <h3>Contact Us</h3>
+        <!-- <img  src="location.png" alt="location"><p>XYZ Organization</img></p> -->
+        <p>Village: Kajrinoorpur, Shahjahanpur</p>
+        <p>Uttar Pradesh, India – 242001</p>
+        <p>Phone: +91-9876543210</p>
+        <p><a href="mailto:info@abccollege.com">Email: info@abccollege.in</a></p>
+      </span>
+
+      <span class="footer-right">
+        <h3>Quick Links</h3>
+        <p><a href="/demo.php">Home</a></p>
+        <p><a href="/about.php">About Us</a></p>
+        <p><a href="/gallery.php">Gallery</a></p>
+        <p><a href="/contact.php">Contact</a></p>
+      </span>
+    </div>
+    
+
+  </footer>
+
+</body>
+</html>
---
+++ D10/header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>header</title>
+  <link rel="stylesheet" href="header.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+
+<body>
+  <header>
+  <span><marquee behavior="slide" direction="left">Knowledge is Power. </marquee></span>
+    <div class="name">     
+      <span><img src="" alt="logo"></span>
+      <span><h3>ABC College</h3></span>
+      <div class="smh">
+        <table>
+          <tr>
+            <td><i class="bi bi-facebook"></i></td>
+            <td><i class="bi bi-whatsapp"></i></td>
+            <td><i class="bi bi-instagram"></i></td>
+          </tr>
+        </table>
+      </div>
+    </div>
+    <ul class="nav">
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About Us</a>
+        <div class='dropdown'>
+          <ul>
+        <li><a href="#">Mission</a></li>
+        <li><a href="#">Vision</a></li>
+          </ul>
+        </div>
+      </li>
+      <li><a href="#">Events</a></li>
+      <li><a href="#">News</a></li>
+      <li><a href="#">Gallery</a></li>
+      <li><a href="#">Contact Us</a></li>
+      <li><a href="#">Apply Now</a></li>
+    </ul>
+  </header>
+</body>
+</html>
